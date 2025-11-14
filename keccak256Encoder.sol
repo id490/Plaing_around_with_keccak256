48a29b1bb4c8e57aeb9ab2960cc9fdbba555e3a0
id490 <75366170+id490@users.noreply.github.com>
2025-11-14 17:29:38 +1100

    Update keccak256Encoder.sol

--- keccak256Encoder.sol
+++ keccak256Encoder.sol
@@ -1,5 +1,5 @@
 pragma solidity ^0.8.0;
-
+// The contract will take an input from a user as a string and encodes it with the relevant cryptography hash function
 contract Enocode_Keccak256 {
     function encodeToKeccak256(string memory input) public pure returns (bytes32) {
         bytes32 hash = keccak256(abi.encodePacked(input));
